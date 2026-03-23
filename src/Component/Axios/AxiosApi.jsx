201cf27af62f64f7be8c893ab741af7e8bb9186c
MD. Owasiul Islam <sanisuper0@gmail.com>
2026-03-23 23:30:04 +0600

    replace the localhost with vercel server link

--- src/Component/Axios/AxiosApi.jsx
+++ src/Component/Axios/AxiosApi.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
   // Replace with your deployed Vercel/Render link later
-  baseURL: "http://localhost:5000", 
+  // baseURL: "http://localhost:5000", 
+  baseURL: "https://teacher-server-zeta.vercel.app/", 
 });
 
 export default axiosPublic;
